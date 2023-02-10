f4588b8426d3f3169941fc36451ba0502d922094
Eduardo J Nunez <96966174+Ejnm@users.noreply.github.com>
2023-02-09 19:41:53 -0500

    Add files via upload

---
+++ GroceryList.sql
@@ -0,0 +1,245 @@
+--
+-- File generated with SQLiteStudio v3.4.1 on Thu Feb 9 19:40:41 2023
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: Store
+CREATE TABLE IF NOT EXISTS Store (
+    id         INTEGER PRIMARY KEY,
+    item       TEXT,
+    section    TEXT,
+    price      INTEGER,
+    popularity INTEGER
+);
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      1,
+                      'salmon',
+                      'frozen',
+                      23.4,
+                      50
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      2,
+                      'banana',
+                      'produce',
+                      0.99,
+                      95
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      3,
+                      'oxiclean',
+                      'hygiene',
+                      11.78,
+                      55
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      4,
+                      'avocado',
+                      'produce',
+                      0.78,
+                      50
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      5,
+                      'lettuce',
+                      'produce',
+                      4.99,
+                      65
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      6,
+                      'cucumber',
+                      'produce',
+                      1.11,
+                      43
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      7,
+                      'chicken',
+                      'meat',
+                      15.18,
+                      35
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      8,
+                      'chicken ground',
+                      'meat',
+                      16.79,
+                      50
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      9,
+                      'broccoli',
+                      'produce',
+                      5.99,
+                      75
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      10,
+                      'pineaple',
+                      'frozen',
+                      2.75,
+                      25
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      11,
+                      'smoothie berries',
+                      'frozen',
+                      2.99,
+                      33
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      12,
+                      'granola',
+                      'bulk',
+                      1.99,
+                      80
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      13,
+                      'vitamin D',
+                      'health',
+                      8.99,
+                      29
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      14,
+                      'potatos',
+                      'produce',
+                      2.99,
+                      26
+                  );
+
+INSERT INTO Store (
+                      id,
+                      item,
+                      section,
+                      price,
+                      popularity
+                  )
+                  VALUES (
+                      15,
+                      'popcorn',
+                      'snacks',
+                      1.99,
+                      47
+                  );
+
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
